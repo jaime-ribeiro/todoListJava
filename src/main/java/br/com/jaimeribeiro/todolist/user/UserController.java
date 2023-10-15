abda91cda8319a75eeca6daee053ca8e5ed51140
Jaime Ribeiro <littlejou@gmail.com>
2023-10-15 13:16:58 -0300

    Adicionando Criptografia na Senha

--- src/main/java/br/com/jaimeribeiro/todolist/user/UserController.java
+++ src/main/java/br/com/jaimeribeiro/todolist/user/UserController.java
@@ -1,5 +1,9 @@
 package br.com.jaimeribeiro.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.name);
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        if(user != null){
+            //Onde está HttpStatus.BAD_REQUEST, também pode ser passado o número do status diretamente
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+
+        }
+        var passwordHashred = BCrypt.withDefaults()
+                .hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
